9741e92acdf3ac55d2ca4d2339995bd358455e48
Dimitris Kougioumtzis <dimkoug@gmail.com>
2021-07-19 10:26:01 +0300

    add product swiper images

--- shopproject/static/js/scripts.js
+++ shopproject/static/js/scripts.js
@@ -40,4 +40,11 @@ $('body').on("click",'.close', function(e){
 })
 
 
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 })
