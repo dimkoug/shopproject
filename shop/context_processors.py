73f2f7d706a09fb14b5bf82d485c741347acfe54
Dimitris <dimkoug@gmail.com>
2022-10-07 21:59:43 +0300

    wip

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -10,40 +10,24 @@ def get_context_data(request):
     product_categories = ProductCategory.objects.select_related(
         'product').values_list('category_id', flat=True).filter(
             product__is_published=True)
-    third_categories = Category.objects.values_list(
-        'id', flat=True).filter(id__in=product_categories)
-    second_categories = Category.objects.filter(children__in=third_categories)
-    first_categories = Category.objects.filter(children__in=second_categories)
+    first_categories = Category.objects.prefetch_related('children__children').filter(
+        children__children__in=product_categories).only('id', 'name').distinct()
     shopping_cart_id = request.session.get('shopping_cart_id')
     heroes = Hero.objects.prefetch_related(
         Prefetch('heroitems',
                  queryset=HeroItem.objects.select_related(
-                    'product', 'hero').filter(
-                        is_published=True), to_attr='item_list'
+                     'product', 'hero').filter(
+                     is_published=True), to_attr='item_list'
                  )
-        ).filter(is_published=True)
+    ).filter(is_published=True)
     if shopping_cart_id:
         basket_count = ShoppingCart.objects.filter(
             shopping_cart_id=shopping_cart_id).count()
     else:
         basket_count = 0
 
-    p = Prefetch(
-        'children',
-        queryset=Category.objects.prefetch_related(
-                    Prefetch(
-                        'children',
-                        queryset=Category.objects.filter(
-                             id__in=third_categories).order_by('order'),
-                    )
-                ).filter(id__in=second_categories).order_by('order'),
-    )
-
-    categories = Category.objects.prefetch_related(p).filter(
-        is_published=True, id__in=first_categories).order_by('order')
-
     return {
-        'categories': categories,
+        'categories': first_categories,
         'tags': Tag.objects.filter(is_published=True),
         'brands': Brand.objects.filter(is_published=True),
         'donottrack': request.META.get('HTTP_DNT') == '1',
