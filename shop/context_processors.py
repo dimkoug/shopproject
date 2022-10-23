210974241a346750eed2614d1425528d38d86d82
Dimitris <dimkoug@gmail.com>
2022-10-23 15:03:51 +0300

    wip

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -7,27 +7,51 @@ from shop.models import (
 
 
 def get_context_data(request):
-    product_categories = ProductCategory.objects.select_related(
-        'product').values_list('category_id', flat=True).filter(
-            product__is_published=True)
-    first_categories = Category.objects.prefetch_related('children__children').filter(
-        children__children__in=product_categories).only('id', 'name').distinct()
+    published_products = ProductCategory.objects.select_related(
+        'product', 'category').filter(product__price__gt=0)
+    active_products = Count('productcategories', filter=published_products)
+    third_categories = Category.objects.prefetch_related(
+            Prefetch('productcategories',
+                     queryset=ProductCategory.objects.select_related('category'))
+            ).annotate(count_active_products=Count('productcategories'),
+                       c=active_products).filter(
+                       productcategories__in=published_products)
+    second_categories = Category.objects.prefetch_related('children').filter(
+        children__in=third_categories).distinct()
+    first_categories = Category.objects.prefetch_related('children').filter(
+        children__in=second_categories).distinct()
     shopping_cart_id = request.session.get('shopping_cart_id')
     heroes = Hero.objects.prefetch_related(
         Prefetch('heroitems',
                  queryset=HeroItem.objects.select_related(
-                     'product', 'hero').filter(
-                     is_published=True), to_attr='item_list'
+                    'product', 'hero').filter(
+                        is_published=True), to_attr='item_list'
                  )
-    ).filter(is_published=True)
+        ).filter(is_published=True)
     if shopping_cart_id:
         basket_count = ShoppingCart.objects.filter(
             shopping_cart_id=shopping_cart_id).count()
     else:
         basket_count = 0
 
+    p = Prefetch(
+        'children',
+        queryset=Category.objects.prefetch_related(
+                    Prefetch(
+                        'children',
+                        queryset=Category.objects.filter(
+                             id__in=third_categories).order_by('order'),
+                        to_attr='third_level'
+                    )
+                ).filter(id__in=second_categories).order_by('order'),
+        to_attr='second_level'
+    )
+
+    categories = Category.objects.prefetch_related(p).filter(
+        is_published=True, id__in=first_categories).order_by('order')
+
     return {
-        'categories': first_categories,
+        'categories': categories,
         'tags': Tag.objects.filter(is_published=True),
         'brands': Brand.objects.filter(is_published=True),
         'donottrack': request.META.get('HTTP_DNT') == '1',
