178d78e01b788ed1c773a44d36d0e43f693e7532
Dimitris Kougioumtzis <dimkoug@gmail.com>
2024-08-28 15:24:27 +0300

    wip

--- shopproject/shipments/forms.py
+++ shopproject/shipments/forms.py
@@ -1,17 +1,41 @@
 from django import forms
 from django.forms import inlineformset_factory
 
+from core.widgets import *
+
 from core.forms import BootstrapForm, BootstrapFormSet
 
-from .models import Shipment
+from warehouses.models import Warehouse
+from shop.models import Product
+from shipments.models import Shipment
 
 
 
 class ShipmentForm(BootstrapForm, forms.ModelForm):
+    warehouse = forms.ModelChoiceField(widget=CustomSelectWithQueryset(ajax_url='/warehouses/sb/'),required=False,queryset=Warehouse.objects.none())
+    product = forms.ModelChoiceField(widget=CustomSelectWithQueryset(ajax_url='/products/sb/'),required=False,queryset=Product.objects.none())
     class Meta:
         model = Shipment
-        fields = ('warehouse', 'product', 'stock', 'date')
+        fields = ('warehouse','product', 'stock', 'date')
 
     def __init__(self, *args, **kwargs):
         request = kwargs.pop('request')
         super().__init__(*args, **kwargs)
+        warehouse_queryset = Warehouse.objects.none()
+        products_queryset = Product.objects.none()
+        if 'warehouse' in self.data:
+            warehouse_queryset = Warehouse.objects.all()
+
+        if 'product' in self.data:
+            products_queryset = Product.objects.all()
+
+        if self.instance.pk:
+            warehouse_queryset = Warehouse.objects.filter(id=self.instance.warehouse_id)
+            products_queryset = Product.objects.filter(id=self.instance.product_id)
+
+        self.fields['warehouse'].queryset = warehouse_queryset
+        self.fields['warehouse'].widget.queryset = warehouse_queryset
+
+
+        self.fields['product'].queryset = products_queryset
+        self.fields['product'].widget.queryset = products_queryset
