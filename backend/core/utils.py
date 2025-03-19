e2e92cecd12ba1d1910f1e1a72fe48042566c18c
Dimitris Kougioumtzis <dimkoug@gmail.com>
2025-03-19 13:03:00 +0200

    wip

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,7 +1,32 @@
+import decimal
 from django.apps import apps
+from django.db import connections
 from django.http import JsonResponse, HttpResponse
 
 from .functions import is_ajax
+
+
+def dictfetchall(cursor):
+    """Return all rows from a cursor as a list of dicts, converting DECIMAL to string while handling NULL values."""
+    columns = [col[0] for col in cursor.description]
+    print(columns)
+    return [
+        {col: (str(value) if isinstance(value, decimal.Decimal) else value) if value is not None else None 
+         for col, value in zip(columns, row)}
+        for row in cursor.fetchall()
+    ]
+
+def run_raw_query_on_db(db_name, sql_query):
+    print(db_name)
+    print(sql_query)
+    
+    with connections[db_name].cursor() as cursor:
+        cursor.execute(sql_query)
+        results = dictfetchall(cursor)
+    
+    return results
+
+
 
 def delete_item(request):
     id = request.GET.get('id')
